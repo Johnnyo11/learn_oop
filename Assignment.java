b26fdcbf381ebc4d9ab6891b05e32a462ebd3371
jonajk111@gmail.com <118031894+Johnnyo11@users.noreply.github.com>
2025-02-01 15:23:21 +0800

    added assignment class, change module to interface

---
+++ Assignment.java
@@ -0,0 +1,9 @@
+public class Assignment implements Module{
+    private String moduleName;
+    private String moduleCode;
+    private int assignmentMarks;
+
+    private void getModuleCode(){
+        return moduleCode;
+    }
+}
--- Module.java
+++ Module.java
@@ -1,15 +1,7 @@
-public class Module {
-    private String modName;
-    private String modCode;
-    private int modAssignmentMarks;
-    private int modExamMarks;
-    private int modTotalMarks;
-
-    public Module(String modName, String modCode, int modAssignmentMarks, int modExamMarks) {
-        this.modName = modName;
-        this.modCode = modCode;
-        this.modAssignmentMarks = modAssignmentMarks;
-        this.modExamMarks = modExamMarks;
-        this.modTotalMarks = modAssignmentMarks + modExamMarks;
-    }
+interface Module {
+    String getModuleName();
+    String getModuleCode();
+    int getModuleAssignmentMarks();
+    int getModExamMarks();
+    int getModuleTotalMarks();
 }
